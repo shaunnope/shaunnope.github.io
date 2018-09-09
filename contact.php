78381646ca196f55de8ea11016e179528ca3b6c7
shaunnope <yap.zhan.hao.sean@gmail.com>
2018-09-09 22:17:31 +0800

    Added contact page to navigator

--- contact.php
+++ contact.php
@@ -9,17 +9,21 @@
     		<ul>
         		<li><a href="/">Home</a></li>
 	        	<li><a href="about.txt">About</a></li>
+						<li><a href="contact.php">Contact Us</a></li>
         		<li><a href="repos.html">Repos</a></li>
         		<li><a href="blog">Blog</a></li>
     		</ul>
 		</nav>
 		<div class="container">
     	<div class="blurb">
-        <h1>Welcome, Habitat!</h1>
+        <h1>Contact Us!</h1>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempus eget ante eu sodales. Donec sed libero egestas purus fringilla sollicitudin. In eget convallis ipsum. Vestibulum at nunc at sem maximus finibus. Sed non dictum ligula, vitae scelerisque velit. Praesent bibendum quis augue ut dapibus. Morbi ipsum velit, interdum sed rhoncus vel, fringilla a diam. Sed ornare sapien vel pellentesque eleifend. Proin scelerisque leo enim, et consectetur magna lacinia ac.</p>
 				<p>In mattis orci vel risus ullamcorper rutrum. Phasellus ornare ullamcorper quam, et mollis nisi auctor nec. Vivamus vehicula convallis eleifend. Cras semper, magna in congue placerat, dolor lorem faucibus nibh, sit amet posuere turpis eros vitae enim. Maecenas eleifend est nulla, sed ornare dui faucibus quis. Duis gravida condimentum scelerisque. Sed at neque orci. Donec maximus placerat felis, nec semper arcu tincidunt ac. Maecenas ac odio in eros placerat viverra ut feugiat urna. Vivamus non tincidunt libero, sed finibus lacus. Cras vulputate ligula eget pretium dictum. Phasellus orci neque, pretium eget auctor quis, ultrices at turpis. Sed sed ultricies eros.</p>
-				<p>I'm best known as a character from <em>Earth, the Movie</em>. Don't trust me. <a href="/about">Read more about my life...</a></p>
-    		</div><!-- /.blurb -->
+				<p>Have any inquiries? Send us an email!</p>
+				<?php
+				echo "Hello World";
+				 ?>
+				</div><!-- /.blurb -->
 				<div class="lMargin">
 					<div id="repoList">
 						<h2>List of Repositories</h2>
