a8f9593c6436e806d3894edd1cf2c86fe4f9d0c7
shaunnope <yap.zhan.hao.sean@gmail.com>
2019-04-07 22:34:29 +0800

    Amended links

--- contact.php
+++ contact.php
@@ -20,9 +20,6 @@
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempus eget ante eu sodales. Donec sed libero egestas purus fringilla sollicitudin. In eget convallis ipsum. Vestibulum at nunc at sem maximus finibus. Sed non dictum ligula, vitae scelerisque velit. Praesent bibendum quis augue ut dapibus. Morbi ipsum velit, interdum sed rhoncus vel, fringilla a diam. Sed ornare sapien vel pellentesque eleifend. Proin scelerisque leo enim, et consectetur magna lacinia ac.</p>
 				<p>In mattis orci vel risus ullamcorper rutrum. Phasellus ornare ullamcorper quam, et mollis nisi auctor nec. Vivamus vehicula convallis eleifend. Cras semper, magna in congue placerat, dolor lorem faucibus nibh, sit amet posuere turpis eros vitae enim. Maecenas eleifend est nulla, sed ornare dui faucibus quis. Duis gravida condimentum scelerisque. Sed at neque orci. Donec maximus placerat felis, nec semper arcu tincidunt ac. Maecenas ac odio in eros placerat viverra ut feugiat urna. Vivamus non tincidunt libero, sed finibus lacus. Cras vulputate ligula eget pretium dictum. Phasellus orci neque, pretium eget auctor quis, ultrices at turpis. Sed sed ultricies eros.</p>
 				<p>Have any inquiries? Send us an email!</p>
-				<?php
-				echo "Hello World";
-				 ?>
 				</div><!-- /.blurb -->
 				<div class="lMargin">
 					<div id="repoList">
