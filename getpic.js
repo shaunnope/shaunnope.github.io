04b5d4594e018ec87641af842acf86e5c75dc510
shaunnope <yap.zhan.hao.sean@gmail.com>
2019-04-07 22:27:12 +0800

    Added Lynxi page

---
+++ getpic.js
@@ -0,0 +1,12 @@
+var name = 'shaunnnope';
+$.ajax({
+type: 'GET',
+ url: 'https://www.instagram.com/' + name,
+ success: function (data) {
+  data = JSON.parse(data.split("window._sharedData = ")[1].split(';')[0]).entry_data.ProfilePage[0];
+  usr_id = data.graphql.user["id"];
+  $.getJSON("https://i.instagram.com/api/v1/users/"+usr_id+"/info/", function(info) {
+    path = info.user.hd_profile_pic_url_info["url"];
+  });
+ }
+});
